92b0ec0a14ba96bcb4d05601f471852560e5a7be
EsjaKS <esja23@ru.is>
2024-03-15 16:52:28 +0000

    ss

--- HW4-mappa/DDL.sql
+++ HW4-mappa/DDL.sql
@@ -1,20 +1,20 @@
 -- Elísabet Erlendsdóttir og Esja Kristín Siggeirsdóttir
 
 DROP TABLE IF EXISTS Languages;
-DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Sponsee;
 DROP TABLE IF EXISTS Subscriber;
+DROP TABLE IF EXISTS Squad;
 DROP TABLE IF EXISTS Teacher;
-DROP TABLE IF EXISTS Registered_to;
-DROP TABLE IF EXISTS Completes;
 DROP TABLE IF EXISTS Learner;
 DROP TABLE IF EXISTS Reviews;
+DROP TABLE IF EXISTS Nominates;
+DROP TABLE IF EXISTS Courses;
+DROP TABLE IF EXISTS Registered_to;
 DROP TABLE IF EXISTS Milestone;
+DROP TABLE IF EXISTS Completes;
 DROP TABLE IF EXISTS Assignment;
 DROP TABLE IF EXISTS Exam;
 DROP TABLE IF EXISTS Question;
-DROP TABLE IF EXISTS Squad;
-DROP TABLE IF EXISTS Sponsee;
-DROP TABLE IF EXISTS Nominates;
 
 
 CREATE TABLE Languages(
@@ -22,6 +22,65 @@ CREATE TABLE Languages(
 	name VARCHAR NOT NULL,
 	speakers VARCHAR NOT NULL,
     PRIMARY KEY(ID)
+);
+
+CREATE TABLE Sponsee(
+    ID INT, 
+    grant_amount INT,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Subscriber(
+	ID INT,
+	name VARCHAR NOT NULL,
+	email VARCHAR NOT NULL,
+	username VARCHAR NOT NULL,
+    PRIMARY KEY(ID),
+    UNIQUE(username, email)
+);
+
+CREATE TABLE Squad(
+    SponseeID INT REFERENCES Sponsee(ID),
+    LanguageID INT REFERENCES Language(ID), -- ath betur
+    ID INT,
+    name VARCHAR NOT NULL,
+    address VARCHAR NOT NULL, -- kannski meira rétt að gera address VARCHAR því það má vera null (stendur optional í verkefnalýsingunni)
+    PRIMARY KEY(ID) 
+);
+
+
+CREATE TABLE Teacher(
+    SubscriberID INT REFERENCES Subscriber(ID),
+    phone VARCHAR NOT NULL,
+    office_hours VARCHAR, -- Can be NULL because some teachers have maybe not worked any hours yet
+    bank VARCHAR NOT NULL,
+    ledger VARCHAR NOT NULL,
+    account_number VARCHAR NOT NULL,
+    PRIMARY KEY(SubscriberID), 
+    UNIQUE(phone)  
+);
+
+CREATE TABLE Learner(
+    SubscriberID INT REFERENCES Subscriber(ID),
+    SponseeID INT REFERENCES sponsee(ID),
+    SquadID INT REFERENCES Squad(ID), -- one to many, ath betur
+    PRIMARY KEY(SubscriberID), 
+    last_login_date DATE NOT NULL,
+    XP INT
+);
+
+CREATE TABLE Reviews(
+    TeacherID INT REFERENCES Teacher(ID),
+    LearnerID INT REFERENCES Learner(ID),
+    stars INT CHECK (stars >= 1 AND stars <= 5), -- Stars are on the scale 1-5
+    CHECK(TeacherID <> LearnerID) -- Check if learner and teacher is the same person, since teacher cannot review himself
+); 
+
+CREATE TABLE Nominates(
+    NominatesID INT NOT NULL REFERENCES Teacher(ID),
+    SponseeID INT REFERENCES Sponsee(ID),
+    year INT,
+    PRIMARY KEY (SponseeID, year)
 );
 
 CREATE TABLE Courses(
@@ -34,31 +93,18 @@ CREATE TABLE Courses(
 	PRIMARY KEY(ID)
 );
 
-CREATE TABLE Subscriber(
-	ID INT,
-	name VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	username VARCHAR NOT NULL,
-    PRIMARY KEY(ID),
-    UNIQUE(username, email)
-);
-
-CREATE TABLE Teacher(
-    SubscriberID INT REFERENCES Subscriber(ID),
-    phone VARCHAR NOT NULL,
-    office_hours VARCHAR, -- Can be NULL because some teachers have maybe not worked any hours yet
-    bank VARCHAR NOT NULL,
-    ledger VARCHAR NOT NULL,
-    account_number VARCHAR NOT NULL,
-    PRIMARY KEY(SubscriberID), --ath betur
-    UNIQUE(phone)  
-);
-
 CREATE TABLE Registered_to(
     CoursesID INT REFERENCES Courses(ID), 
     LearnerID INT REFERENCES Learner(ID), 
     PRIMARY KEY(CoursesID, LearnerID) 
 ); 
+
+CREATE TABLE Milestone(
+    CoursesID INT REFERENCES Courses(ID),
+    ID INT,
+    credits INT,
+    PRIMARY KEY(ID) 
+);
 
 CREATE TABLE Completes(
     CoursesID INT REFERENCES Courses(ID),
@@ -67,29 +113,6 @@ CREATE TABLE Completes(
     grade VARCHAR NOT NULL,
     FOREIGN KEY(CoursesID, LearnerID) INT REFERENCES Registered_to(CoursesID, LearnerID),  
     PRIMARY KEY(CoursesID, LearnerID, MilestoneID)
-);
-
-CREATE TABLE Learner(
-    SubscriberID INT REFERENCES Subscriber(ID),
-    SponseeID INT REFERENCES sponsee(ID),
-    SquadID INT REFERENCES Squad(ID), -- one to many, ath betur
-    PRIMARY KEY(SubscriberID), -- ath betur 
-    last_login_date DATE NOT NULL,
-    XP INT
-);
-
-CREATE TABLE Reviews(
-    TeacherID INT REFERENCES Teacher(ID),
-    LearnerID INT REFERENCES Learner(ID),
-    stars INT CHECK (stars >= 1 AND stars <= 5), -- Stars are on the scale 1-5
-    CHECK(TeacherID <> LearnerID) -- Check if learner and teacher is the same person, since teacher cannot review himself
-); 
-
-CREATE TABLE Milestone(
-    CoursesID INT REFERENCES Courses(ID),
-    ID INT,
-    credits INT,
-    PRIMARY KEY(ID) 
 );
 
 CREATE TABLE Assignment(
@@ -107,33 +130,10 @@ CREATE TABLE Exam(
 
 -- Weak entity
 CREATE TABLE Question(
-	ID INT REFERENCES Exams,
+	ID INT REFERENCES Exams, --id
     number VARCHAR,
     weight VARCHAR,
     text VARCHAR NOT NULL,
     PRIMARY KEY(number),
-    FOREIGN KEY(ExamID) REFERENCES Exam(ID)
+    FOREIGN KEY(ExamID) REFERENCES Exam(ID) -- taka ut
 );
-
-CREATE TABLE Squad(
-    SponseeID INT REFERENCES Sponsee(ID),
-    ID INT,
-    LanguageID INT REFERENCES Language(ID), -- ath betur
-    name VARCHAR NOT NULL,
-    address VARCHAR NOT NULL, -- kannski meira rétt að gera address VARCHAR því það má vera null (stendur optional í verkefnalýsingunni)
-    PRIMARY KEY(ID) 
-);
-
-CREATE TABLE Sponsee(
-    ID INT, 
-    grant_amount INT,
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE Nominates(
-    NominatesID INT NOT NULL REFERENCES Teacher(ID),
-    SponseeID INT REFERENCES Party(ID),
-    year INT,
-    PRIMARY KEY (SponseeID, year)
-);
-
